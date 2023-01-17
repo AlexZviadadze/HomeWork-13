973685d2a357d212e1122a8fa770e4449b982f51
AlexZviadadze <117842458+AlexZviadadze@users.noreply.github.com>
2023-01-17 10:59:58 +0400

    Created html  css and js
    I  finished  js  file

---
+++ script/script.js
@@ -0,0 +1,36 @@
+//1
+let user ="Aex";
+let surName = "Zviadadze";
+let age = 25;
+console.log(user +  " " + surName +" " +age+ "age");
+// 2
+let x  =  7;
+let y = 13;
+let sum = x + y;
+let sub = x - y;
+let div = x % y;
+let mul = x * y;
+
+console.log(mul);
+console.log(div);
+console.log(sub);
+console.log(sum);
+
+// 3
+
+let length = 7;
+let width = 5;
+
+let area = length * width;
+console.log(area)
+
+
+// 4
+
+let radius = 7;
+const PI = Math.PI;
+let circleArea;
+
+circleArea = PI * radius*radius;
+
+console.log(circleArea);
